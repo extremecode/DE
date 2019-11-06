5d7c27acc1ad4aa5ecece6d36a2acb208c39ebf4
AKASH GUPTA <akash.gupta2@microfocus.com>
2019-11-06 16:09:43 +0530

    algoDs first commit

---
+++ code-basics/data-structures/src/test/java/com/de/code/basics/ds/stack/StackTest.java
@@ -0,0 +1,25 @@
+package com.de.code.basics.ds.stack;
+
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StackTest {
+
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void stack() {
+        assertTrue(true);
+    }
+}
