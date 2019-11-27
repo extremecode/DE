a206a7f970ca423d8ef8992caefc46b759f35e55
AKASH GUPTA <akash.gupta2@microfocus.com>
2019-11-27 14:31:06 +0530

    bfs and dfs

---
+++ code-basics/algorithms/src/test/java/com/de/code/basics/algorithms/graph/BFSTest.java
@@ -0,0 +1,41 @@
+package com.de.code.basics.algorithms.graph;
+
+import com.de.code.basics.algorithms.ConsoleFixture;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+import static org.junit.Assert.*;
+
+public class BFSTest extends ConsoleFixture {
+
+    private BFS bfs;
+
+    @Before
+    public void setUp() throws Exception {
+       super.setUp();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    @Test
+    public void traverse() {
+        Graph g = new Graph(4);
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(1, 2);
+        g.addEdge(2, 0);
+        g.addEdge(2, 3);
+        g.addEdge(3, 3);
+        bfs = new BFS();
+        bfs.init(g).traverse(2);
+        assertEquals("2 \n" +
+                "0 \n" +
+                "3 \n" +
+                "1 \n", console());
+    }
+}
