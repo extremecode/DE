deee62c2bf23662ca4509269ec4ce831ac878ab3
AKASH GUPTA <akash.gupta2@microfocus.com>
2020-02-19 10:58:01 +0530

    algorithms for trees and graph

---
+++ code-basics/algorithms/src/main/java/com/de/code/basics/algorithms/trees/BottomView.java
@@ -0,0 +1,40 @@
+package com.de.code.basics.algorithms.trees;
+
+import com.de.code.basics.algorithms.ds.Queue;
+
+import java.util.TreeMap;
+
+public class BottomView {
+    Node<Integer> root;
+
+    int hd = 0;
+
+    public void display() {
+        TreeMap<Integer,Integer> map = new TreeMap<>();
+
+        Queue<Node> queue = new Queue<>();
+
+        root.hd = hd;
+        queue.add(root);
+
+        while (!queue.isEmpty()){
+            Node temp = queue.remove();
+            map.put(temp.hd,(Integer) temp.data);
+
+            hd = temp.hd;
+            if(temp.left!=null){
+                temp.left.hd = temp.hd-1;
+                queue.add(temp.left);
+            }
+
+            if(temp.right!=null){
+                temp.right.hd = temp.hd+1;
+                queue.add(temp.left);
+            }
+        }
+
+        map.forEach((k,v)->{
+            System.out.println(v);
+        });
+    }
+}
