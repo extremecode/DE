3a64442f054c0443327c20a6eb156931e03fbc14
AKASH GUPTA <akash.gupta2@microfocus.com>
2020-05-02 20:42:30 +0530

    info commit

--- code-basics/multi-threadng/src/main/java/com/de/code/basics/mt/bp/ConcurrentTask.java
+++ code-basics/multi-threadng/src/main/java/com/de/code/basics/mt/bp/ConcurrentTask.java
@@ -7,12 +7,13 @@ import java.util.List;
 public class ConcurrentTask implements Runnable {
 
     private static List TMEP = Collections.synchronizedList(new ArrayList<>());
+
     @Override
     public void run() {
-        int count=1;
-        TMEP.add("Its Working "+count++);
-        TMEP.add("Its Working "+count++);
-        TMEP.forEach(t->{
+        int count = 1;
+        TMEP.add("Its Working " + count++);
+        TMEP.add("Its Working " + count++);
+        TMEP.forEach(t -> {
             System.out.println(t);
         });
 
