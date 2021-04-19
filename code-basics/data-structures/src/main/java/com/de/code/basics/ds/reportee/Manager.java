5b63153241d91d25c13b47e5c11638fdb671086c
AKASH GUPTA <akash.gupta2@microfocus.com>
2021-04-19 14:17:55 +0530

    Employee Reportee problem

--- code-basics/data-structures/src/main/java/com/de/code/basics/ds/reportee/Manager.java
+++ code-basics/data-structures/src/main/java/com/de/code/basics/ds/reportee/Manager.java
@@ -9,15 +9,13 @@ public class Manager {
     Set<Employee> getAllReporteeEmployee(final Employee mgr,final Set<Employee> allEmployees) {
         final Set<Employee> allReporteeEmployee = new HashSet<>();
         final Set<Employee> firstLevelReporteeEmployess = new HashSet<>();
-        AtomicBoolean isManager = new AtomicBoolean(false);
         allEmployees.forEach(employee -> {
             if (employee.getManager()!=null && employee.getManager().equals(mgr)) {
                 firstLevelReporteeEmployess.add(employee);
-                isManager.set(true);
             }
         });
-        if(!isManager.get())
-            return allReporteeEmployee;
+        if(firstLevelReporteeEmployess.isEmpty())
+            return firstLevelReporteeEmployess;
         firstLevelReporteeEmployess.forEach(employee -> {
             allReporteeEmployee.addAll(getAllReporteeEmployee(employee, allEmployees));
         });
